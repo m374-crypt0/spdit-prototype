9ad48a9df713400b64b8a4a6c5de6273f5f4ad70
Sebastien Levy <m374-crypt0@pm.me>
2026-04-30 18:22:04 +0000

    test(stochastic): distributions
    uint64 chi-square test

--- src/stochastic/distributions.ts
+++ src/stochastic/distributions.ts
@@ -1,17 +1,34 @@
 import type { UniformRandomBitGenerator } from "./uniformRandomBitGenerators";
 
-export class UniformUint64<State, Value> {
-  constructor(urbg: UniformRandomBitGenerator<State, Value>) { }
+export class UniformUint64<State, Value extends number | bigint> {
+  constructor(urbg: UniformRandomBitGenerator<State, Value>) {
+    this.urbg = urbg
+  }
 
   newUint64(range: [bigint, bigint]) {
-    const [first, last] = range
+    const [min, max] = [
+      range[0] < range[1] ? range[0] : range[1],
+      range[1] > range[0] ? range[1] : range[0]]
 
-    if ((last & this.UINT64_MAX) != last || (first & this.UINT64_MAX) != first)
+    if ((max & this.UINT64_MAX) != max || (min & this.UINT64_MAX) != min)
       throw new Error('range overflow uint64')
 
-    return 11n;
+    const r = max - min + 1n
+
+    while (true) {
+      const x = this.urbg.newValue()
+      const m = BigInt(x) * r
+      const low = m & this.UINT64_MAX // 65 bits
+      const threshold = (this.UINT65 - r) % r
+
+      if (low >= threshold)
+        return min + (m >> 64n)
+    }
   }
 
   // TODO: refactor, common everywhere
   readonly UINT64_MAX: bigint = (1n << 64n) - 1n
+  readonly UINT65: bigint = 1n << 64n
+
+  private urbg: UniformRandomBitGenerator<State, Value>
 }
--- test/stochastic/distributions.test.ts
+++ test/stochastic/distributions.test.ts
@@ -23,5 +23,28 @@ describe('distributions test suite', () => {
       expect(() => { distribution.newUint64([1n << 62n, 1n << 70n]) }).toThrowError("range overflow uint64")
       expect(() => { distribution.newUint64([1n << 68n, 1n << 56n]) }).toThrowError("range overflow uint64")
     })
+
+    it('should produce a uniform distribution over a given range (chi-square goodness-of-fit)', () => {
+      const seedGenerator: SeedGenerator<bigint> = new SplitMix64(42n)
+      const urbg: UniformRandomBitGenerator<[bigint, bigint], bigint> = new Xoroshiro128Plus(seedGenerator)
+      const distribution = new UniformUint64(urbg)
+
+      const bins = 100
+      const samples = 100_000
+      const range: [bigint, bigint] = [0n, BigInt(bins - 1)]
+
+      const occurences = new Array(bins).fill(0)
+
+      Array
+        .from({ length: samples }, () => distribution.newUint64(range))
+        .forEach(value => occurences[Number(value)]!++)
+
+      const expected = samples / bins
+      const chiSquare = occurences
+        .reduce((sum, o) => sum + (o - expected) ** 2 / expected, 0)
+
+      // df = bins - 1 = 99, alpha = 0.01 => critical value = 135.807
+      expect(chiSquare).toBeLessThan(135.807)
+    })
   })
 })
