a56d1e9452f750283c6467331b90edfef219e87e
Sebastien Levy <m374-crypt0@pm.me>
2026-05-04 11:41:30 +0000

    test(SPD): lane concept within buffer
    Each buffer has *n* lane, *n* being either 16 or 256 depending the lane
    is *low* or *high* respectively
    Each lane contain values between 0 and *n*-1

--- src/SPD/create.ts
+++ src/SPD/create.ts
@@ -2,23 +2,28 @@ export class SPD {
   constructor(type: 'low' | 'high') {
     this.laneSize = type === 'low' ? 16 : 256
     this.size = this.laneSize ** 2
-    this.buffer = new ArrayBuffer(this.size)
+    this.buffer = new ArrayBuffer(this.size, { maxByteLength: this.size })
+    this.bufferView = Buffer.from(this.buffer)
+
+    this.bufferView
+      .forEach((_, i) => this.bufferView.writeUint8(i % this.laneSize, i))
   }
 
   readonly [Symbol.iterator] = () => {
     const b = this.buffer
     const ls = this.laneSize
 
-    function* getLane() {
+    function* getLanes() {
       for (let i = 0; i < ls; i++)
         yield Iterator.from(Buffer.from(b)).drop(i * ls).take(ls)
     }
 
-    return getLane()
+    return getLanes()
   }
 
   readonly laneSize: number
   readonly size: number
+  readonly bufferView: Buffer<ArrayBuffer>
 
   private buffer: ArrayBuffer
 }
--- test/SPD/create.test.ts
+++ test/SPD/create.test.ts
@@ -11,6 +11,23 @@ describe('SPD test suite', () => {
         expect(spd.size).toBe(spd.laneSize ** 2)
         expect(Iterator.from(spd).toArray().length).toBe(spd.laneSize)
       })
+
+    it('should, for a low SPD, having 16 lanes containing values in [0, 15]', () => {
+      const spd = new SPD('low')
+
+      const areValuesInRange = Iterator.from(spd)
+        .flatMap(laneIterator =>
+          laneIterator.map(v => v >= 0 && v <= 15))
+        .reduce((acc, cur) => acc && cur, true)
+
+      const areAllValuesNotZero = Iterator.from(spd)
+        .map(laneIterator =>
+          laneIterator.reduce((acc, cur) => acc + cur, 0))
+        .reduce((acc, cur) => acc + cur, 0) > 0
+
+      expect(areValuesInRange).toBeTrue()
+      expect(areAllValuesNotZero).toBeTrue()
+    })
   })
 })
 
