f3f2c77a3f2eb1ee57d56f4f91d4014cdf80e36b
Sebastien Levy <m374-crypt0@pm.me>
2026-05-12 11:46:46 +0000

    feat(exchange): exchanger rewrite
    bunch of simplification to demonstrate high SPD secret exchange protocol

--- src/exchange/exchanger.ts
+++ src/exchange/exchanger.ts
@@ -1,97 +1,17 @@
-import type { Initiator, Recipient } from "."
+import type { Initiator, Recipient } from "./peers";
 
 export class Exchanger {
-  constructor(options: Options) {
-    const { initiator, recipient } = options
-
-    if (initiator.identifier === recipient.identifier)
-      throw new Error('invalid exchange configuration, initiator must be different from recipient')
-
-    this.initiator = initiator
-    this.recipient = recipient
-
-    this.state_ = 'not_started'
+  constructor(options: ExchangerOptions) {
+    if (options.recipient.identifier === options.initiator.identifier)
+      throw new Error('invalid peers specified')
   }
 
-  state(): State {
-    return this.state_
+  state() {
+    return 'not_started'
   }
-
-  initiate() {
-    if (this.state_ !== 'not_started')
-      throw new Error('invalid initiate call')
-
-    this.state_ = 'initiating'
-
-    return new Promise<void>(resolve => {
-      // NOTE: Forces to wait the next turn of the event loop to simulate an
-      // eventual settle of this promise
-      setImmediate(() => {
-        const { encodedEntropySource, seed } = this.initiator.generateInitiateExchangeData()
-        const { encodedPayload } = this.recipient.generateEncodedPayload(seed, encodedEntropySource)
-
-        this.encodedPayload = encodedPayload
-        this.seed = seed
-        this.state_ = 'initiated'
-
-        resolve()
-      })
-    })
-  }
-
-  compute() {
-    if (this.state_ !== 'initiated')
-      throw new Error('invalid compute call')
-
-    this.state_ = 'computing'
-
-    return new Promise<void>(resolve => {
-      // NOTE: Forces to wait the next turn of the event loop to simulate an
-      // eventual settle of this promise
-      setImmediate(() => {
-        this.initiator.reconstructLowSPD(this.encodedPayload!)
-        this.state_ = 'ready'
-
-        resolve()
-      })
-    })
-  }
-
-  finalize() {
-    if (this.state_ !== 'ready')
-      throw new Error('invalid finalize call')
-
-    this.state_ = 'finalizing'
-
-    return new Promise<void>(resolve => {
-      setImmediate(() => {
-        const { encodedHighSPD } = this.initiator.generateFinalizeExchangeData()
-        this.recipient.acceptEncodedHighSPD(encodedHighSPD)
-        this.state_ = 'finalized'
-
-        resolve()
-      })
-    })
-  }
-
-  seed?: bigint
-
-  private state_: State
-  private initiator: Initiator
-  private recipient: Recipient
-  private encodedPayload?: Readonly<Buffer<ArrayBuffer>>
 }
 
-type Options = {
+type ExchangerOptions = {
   initiator: Initiator,
   recipient: Recipient
 }
-
-type State =
-  'not_started'
-  | 'initiating'
-  | 'initiated'
-  | 'computing'
-  | 'ready'
-  | 'finalizing'
-  | 'finalized'
--- test/exchange/exchanger.test.ts
+++ test/exchange/exchanger.test.ts
@@ -1,200 +1,25 @@
 import { Exchanger, Initiator, Recipient } from "src/exchange";
-import { Transcoder, SPD } from "src/transcoding";
 
-import { beforeEach, describe, expect, it, spyOn } from "bun:test";
+import { describe, expect, it } from "bun:test";
 
-describe('SPD test suite', () => {
-  describe('exchange test suite', () => {
+describe('exchange test suite', () => {
+  describe('exchanger instantiation', () => {
     describe('exchanger instantiation', () => {
-      it('should throw at initializing an exchange between an intiator and himself', () => {
+      it('should throw when instantiating an exchanger with peers having the same identifier', () => {
         const initiator = new Initiator('alice')
         const recipient = new Recipient('alice')
 
         expect(() => new Exchanger({ initiator, recipient }))
-          .toThrowError('invalid exchange configuration, initiator must be different from recipient')
+          .toThrowError('invalid peers specified')
       })
 
-      it('should report the state of an exchange as not_started when instantiated', () => {
-        const initiator = new Initiator('alice')
-        const recipient = new Recipient('bob')
-        expect(new Exchanger({ initiator, recipient }).state()).toBe('not_started')
-      })
-    })
-
-    describe('exchange flow', () => {
-      let exchanger: Exchanger
-      let initiator: Initiator
-      let recipient: Recipient
-
-      describe('initiator and recipient share the same low SPD', () => {
-        const transcoder = new Transcoder({ lowSPD: new SPD('low') })
-
-        describe('from not_started', () => {
-          beforeEach(() =>
-            exchanger = new Exchanger({
-              initiator: new Initiator('initiator', transcoder),
-              recipient: new Recipient('recipient', transcoder)
-            }))
-
-          it('should throw if initiate is called while state is not not_started', () => {
-            exchanger.initiate()
-
-            expect(() => exchanger.initiate())
-              .toThrowError('invalid initiate call')
-          })
-
-          it('should initiate exchange between initiator and recipient then transition to initiating', async () => {
-            exchanger.initiate()
-
-            expect(exchanger.state()).toBe('initiating')
-          })
+      it('should initialize the exchanger state to not_started at instantitation', () => {
+        const exchanger = new Exchanger({
+          initiator: new Initiator('alice'),
+          recipient: new Recipient('bob')
         })
 
-        describe('from initiating', () => {
-          let initiating: Promise<void>
-
-          beforeEach(() => {
-            initiator = new Initiator('initiator', transcoder)
-            recipient = new Recipient('recipient', transcoder)
-            exchanger = new Exchanger({ initiator, recipient })
-            initiating = exchanger.initiate()
-          })
-
-          it('should eventually transition from initiating to initiated', async () => {
-            const generateInitiateExchangeData = spyOn(initiator, 'generateInitiateExchangeData')
-            const generateEncodedPayload = spyOn(recipient, 'generateEncodedPayload')
-
-            await initiating
-
-            expect(generateInitiateExchangeData).toHaveBeenCalled()
-            expect(generateEncodedPayload).toHaveBeenCalled()
-
-            expect(exchanger.state()).toBe('initiated')
-          })
-        })
-
-        describe('from initiated', () => {
-          beforeEach(async () => {
-            initiator = new Initiator('initiator', transcoder)
-            recipient = new Recipient('recipient', transcoder)
-            exchanger = new Exchanger({ initiator, recipient })
-            await exchanger.initiate()
-          })
-
-          it('should throw if ask for compute is done if state is not initiated', () => {
-            const exchanger = new Exchanger({
-              initiator: new Initiator('initiator'),
-              recipient: new Recipient('recipient')
-            })
-
-            exchanger.initiate()
-
-            expect(() => exchanger.compute()).toThrowError('invalid compute call')
-          })
-
-          it('should transition from initiated to computing', () => {
-            exchanger.compute()
-
-            expect(exchanger.state()).toBe('computing')
-          })
-        })
-
-        describe('from computing', () => {
-          let computing: Promise<void>
-
-          beforeEach(async () => {
-            initiator = new Initiator('initiator', transcoder)
-            recipient = new Recipient('recipient', transcoder)
-            exchanger = new Exchanger({ initiator, recipient })
-
-            await exchanger.initiate()
-            computing = exchanger.compute()
-          })
-
-          it('should eventually transition from computing to ready', async () => {
-            const reconstructLowSPD = spyOn(initiator, 'reconstructLowSPD')
-
-            await computing
-
-            expect(reconstructLowSPD).toHaveBeenCalled()
-            expect(exchanger.state()).toBe('ready')
-          })
-        })
-
-        describe('from ready', () => {
-          beforeEach(async () => {
-            initiator = new Initiator('initiator', transcoder)
-            recipient = new Recipient('recipient', transcoder)
-            exchanger = new Exchanger({ initiator, recipient })
-
-            await exchanger.initiate()
-            await exchanger.compute()
-          })
-
-          it('should throw asking for finalize the exchange if not ready', async () => {
-            const exchanger = new Exchanger({
-              initiator: new Initiator('initiator'),
-              recipient: new Recipient('recipient')
-            })
-
-            await exchanger.initiate()
-            exchanger.compute()
-
-            expect(() => exchanger.finalize()).toThrowError('invalid finalize call')
-          })
-
-          it('should transition from ready to finalizing', () => {
-            exchanger.finalize()
-
-            expect(exchanger.state()).toBe('finalizing')
-          })
-        })
-
-        describe('from finalizing', () => {
-          let finalizing: Promise<void>
-
-          beforeEach(async () => {
-            initiator = new Initiator('initiator', transcoder)
-            recipient = new Recipient('recipient', transcoder)
-            exchanger = new Exchanger({ initiator, recipient })
-
-            await exchanger.initiate()
-            await exchanger.compute()
-            finalizing = exchanger.finalize()
-          })
-
-          it('should eventually transition from finalizing to finalized', async () => {
-            const generateFinalizeExchangeData = spyOn(initiator, 'generateFinalizeExchangeData')
-            const acceptEncodedHighSPD = spyOn(recipient, 'acceptEncodedHighSPD')
-
-            await finalizing
-
-            expect(generateFinalizeExchangeData).toHaveBeenCalled()
-            expect(acceptEncodedHighSPD).toHaveBeenCalled()
-            expect(exchanger.state()).toBe('finalized')
-          })
-        })
-
-        describe('from finalized', () => {
-          beforeEach(async () => {
-            initiator = new Initiator('initiator', transcoder)
-            recipient = new Recipient('recipient', transcoder)
-            exchanger = new Exchanger({ initiator, recipient })
-
-            await exchanger.initiate()
-            await exchanger.compute()
-            await exchanger.finalize()
-          })
-
-          it('should transcode a same message from initiator to recipient', async () => {
-            const message = Buffer.from(Math.random().toString())
-
-            const encoded = initiator.transcoder().encode(message)
-            const decoded = recipient.transcoder().decode(encoded)
-
-            expect(decoded).toEqual(message)
-          })
-        })
+        expect(exchanger.state()).toBe('not_started')
       })
     })
   })
