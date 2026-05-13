6ae35ef569db906df9c663822e71a76f66a76fa3
Sebastien Levy <m374-crypt0@pm.me>
2026-05-13 15:43:44 +0000

    feat(hashing): creation
    seeded instantiation

--- src/hashing/shi7.ts
+++ src/hashing/shi7.ts
@@ -1,5 +1,13 @@
+import { SplitMix64 } from "src/stochastic"
+
 export class Shi7 {
-  seed() {
-    return Math.random()
+  constructor(seed?: bigint) {
+    this.seed_ = seed ?? new SplitMix64().state()
   }
+
+  seed() {
+    return this.seed_
+  }
+
+  private seed_?: bigint
 }
--- test/hashing/create.test.ts
+++ test/hashing/create.test.ts
@@ -1,5 +1,6 @@
 import { describe, expect, it } from "bun:test";
 import { Shi7 } from "src/hashing";
+import { SplitMix64 } from "src/stochastic";
 
 describe('hashing test suite', () => {
   describe('hashing function instantiation', () => {
@@ -7,6 +8,14 @@ describe('hashing test suite', () => {
       const f1 = new Shi7, f2 = new Shi7
 
       expect(f1.seed()).not.toBe(f2.seed())
+    })
+
+    it('should give the same seed if instantiated so', () => {
+      const g = new SplitMix64
+      const f1 = new Shi7(g.state()),
+        f2 = new Shi7(g.state())
+
+      expect(f1.seed()).toBe(f2.seed())
     })
   })
 })
