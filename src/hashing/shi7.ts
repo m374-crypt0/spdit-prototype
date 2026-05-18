e2c72362c9664275eb0ad17d94d9366a33f35fcb
Sebastien Levy <m374-crypt0@pm.me>
2026-05-18 09:28:24 +0000

    feat(hashing): instantiation of Shi7

--- src/hashing/shi7.ts
+++ src/hashing/shi7.ts
@@ -1,1 +1,32 @@
-export class Shi7 { }
+import { SplitMix64 } from "src/stochastic"
+import { SPD } from "src/transcoding"
+
+export class Shi7 {
+  constructor(options?: Options) {
+    this.seed_ = options?.seed ?? new SplitMix64().state()
+
+
+    this.hashBitSize_ = options?.hashBitSize ?? 256
+  }
+
+  hashBitSize() {
+    return this.hashBitSize_
+  }
+
+  seed() {
+    return this.seed_
+  }
+
+  highSPD(): Readonly<SPD> {
+    return this.highSPD_ = this.highSPD_ ?? new SPD('high', { kind: 'seed', seed: this.seed_ })
+  }
+
+  private seed_: bigint
+  private hashBitSize_: number
+  private highSPD_?: SPD
+}
+
+type Options = {
+  seed?: bigint,
+  hashBitSize?: 64 | 128 | 256 | 512 | 1024
+}
--- test/hashing/create.test.ts
+++ test/hashing/create.test.ts
@@ -1,4 +1,54 @@
+import { Shi7 } from "src/hashing";
+
 import { describe, expect, it } from "bun:test";
 
-describe('hashing test suite', () => { })
+describe('hashing test suite', () => {
+  describe('shi7 instantiation', () => {
+    it('should default instantiate shi7 with 256 hash bit size and random seed', () => {
+      const hasher = new Shi7
+
+      expect(hasher.hashBitSize()).toBe(256)
+      expect(hasher.seed()).toBeDefined()
+    })
+
+    it('should be instantiated with a default hash bit size and a specified seed', () => {
+      const hasher = new Shi7({ seed: 0n })
+
+      expect(hasher.hashBitSize()).toBe(256)
+      expect(hasher.seed()).toBe(0n)
+    })
+
+    it('should be instantiated with a specific hash bit size and a random seed', () => {
+      const hasher = new Shi7({ hashBitSize: 64 })
+
+      expect(hasher.hashBitSize()).toBe(64)
+      expect(hasher.seed()).toBeDefined()
+    })
+
+    it('should expose a readonly high SPD at instantiation, whose content depends on seed', () => {
+      const hasher1 = new Shi7({ seed: 42n })
+      const hasher2 = new Shi7({ seed: 43n })
+      const hasher3 = new Shi7({ seed: 42n })
+
+      expect(hasher1.highSPD().readonlyBufferView()).not.toEqual(hasher2.highSPD().readonlyBufferView())
+      expect(hasher1.highSPD().readonlyBufferView()).toEqual(hasher3.highSPD().readonlyBufferView())
+    })
+  })
+
+  describe('hashing empty message', () => {
+    describe('collision resistance', () => { })
+    describe('pre-image attacks resistance', () => { })
+    describe('diffusion properties', () => { })
+  })
+  describe('hashing small messages in regard of hash bit size', () => {
+    describe('collision resistance', () => { })
+    describe('pre-image attacks resistance', () => { })
+    describe('diffusion properties', () => { })
+  })
+  describe('hashing big messages in regard of hash bit size', () => {
+    describe('collision resistance', () => { })
+    describe('pre-image attacks resistance', () => { })
+    describe('diffusion properties', () => { })
+  })
+})
 
