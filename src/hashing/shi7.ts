4959ee3ad1cad95d0a6b4790dd490de202fa39c6
Sebastien Levy <m374-crypt0@pm.me>
2026-05-18 18:59:03 +0000

    test(hashing): empty message
    ensured good hash function properties for empty messages

--- src/hashing/shi7.ts
+++ src/hashing/shi7.ts
@@ -1,5 +1,5 @@
-import { SplitMix64 } from "src/stochastic"
-import { SPD } from "src/transcoding"
+import { shuffleBuffer, SplitMix64, UniformUint64, Xoroshiro128Plus } from "src/stochastic"
+import { SPD, Transcoder } from "src/transcoding"
 
 export class Shi7 {
   constructor(options?: Options) {
@@ -21,12 +21,44 @@ export class Shi7 {
     return this.highSPD_ = this.highSPD_ ?? new SPD('high', { kind: 'seed', seed: this.seed_ })
   }
 
+  hash(buffer: Readonly<Buffer<ArrayBuffer>>) {
+    const isMessageEmpty = buffer.byteLength === 0
+
+    const message = isMessageEmpty ? this.highSPD().readonlyBufferView() : buffer
+
+    const transcoder = new Transcoder({ highSPD: this.highSPD() })
+
+    const preHash = this.encodeUntilSizeInBits(message, transcoder, this.hashBitSize_ * SPD.DIMENSIONAL_FACTOR)
+    const seedAsBuffer = this.encodeUntilSizeInBits(preHash, transcoder, 64)
+    const seed = BigInt(`0x${seedAsBuffer.toHex()}`)
+
+    shuffleBuffer(preHash, new UniformUint64(new Xoroshiro128Plus(new SplitMix64(seed))))
+
+    if (isMessageEmpty)
+      shuffleBuffer(preHash, new UniformUint64(new Xoroshiro128Plus(new SplitMix64(seed))))
+
+    const hashAsBuffer = transcoder.decode(preHash)
+
+    return BigInt(`0x${hashAsBuffer.toHex()}`)
+  }
+
+  private encodeUntilSizeInBits(buffer: Readonly<Buffer<ArrayBuffer>>, transcoder: Transcoder, size: number) {
+    let b: Buffer<ArrayBuffer> = buffer
+
+    while (b.byteLength > size)
+      b = transcoder.decode(b)
+
+    return b
+  }
+
   private seed_: bigint
-  private hashBitSize_: number
+  private hashBitSize_: HashBitSize
   private highSPD_?: SPD
 }
 
+type HashBitSize = 64 | 128 | 256 | 512 | 1024
+
 type Options = {
   seed?: bigint,
-  hashBitSize?: 64 | 128 | 256 | 512 | 1024
+  hashBitSize?: HashBitSize
 }
--- test/hashing/create.test.ts
+++ test/hashing/create.test.ts
@@ -1,6 +1,8 @@
 import { Shi7 } from "src/hashing";
+import { SplitMix64 } from "src/stochastic";
+import { bitwiseDiffusion } from "./utils";
 
-import { describe, expect, it } from "bun:test";
+import { afterAll, beforeAll, describe, expect, it } from "bun:test";
 
 describe('hashing test suite', () => {
   describe('shi7 instantiation', () => {
@@ -36,10 +38,92 @@ describe('hashing test suite', () => {
   })
 
   describe('hashing empty message', () => {
-    describe('collision resistance', () => { })
-    describe('pre-image attacks resistance', () => { })
-    describe('diffusion properties', () => { })
+    const empty = Buffer.from(new ArrayBuffer(0))
+
+    it('should give the same value for the same Shi7 instance', () => {
+      const hasher1 = new Shi7
+      const hasher2 = new Shi7({ hashBitSize: hasher1.hashBitSize(), seed: hasher1.seed() })
+
+      expect(hasher1.hash(empty)).toBe(hasher2.hash(empty))
+    })
+
+    describe('collision resistance', () => {
+      let hashes: Set<bigint>
+
+      beforeAll(() => hashes = new Set<bigint>)
+
+      it.each(([64, 128, 256, 512, 1024] as const)
+        .map(hashBitSize =>
+          Array.from({ length: 10 }, () => new SplitMix64().newSeed())
+            .map(seed => ({ seed, hashBitSize }))
+        )
+        .flat())
+        ('should not collide hash values between different Shi7 instances', ({ seed, hashBitSize }) => {
+          hashes.add(new Shi7({ seed, hashBitSize }).hash(empty))
+        })
+
+      afterAll(() => expect(hashes.size).toBe(50))
+    })
+
+    describe('diffusion properties', () => {
+      let hashes: Array<bigint>
+
+      beforeAll(() => hashes = [])
+
+      it.each(([64, 128, 256, 512, 1024] as const)
+        .map(hashBitSize =>
+          Array.from({ length: 10 }, () => new SplitMix64().newSeed())
+            .map(seed => ({ seed, hashBitSize }))
+        )
+        .flat())
+        ('should give pretty different hashes for seeds differing by one bit', ({ seed, hashBitSize }) => {
+          hashes.push(new Shi7({ seed, hashBitSize }).hash(empty))
+        })
+
+      afterAll(() => {
+        const diffusionResults = new Array<number>
+
+        for (let i = 0; i < hashes.length; i += 2)
+          diffusionResults.push(bitwiseDiffusion(hashes[i]!, hashes[i + 1]!))
+
+        const diffusionMean = diffusionResults
+          .reduce((acc, cur) => acc + cur, 0) / diffusionResults.length
+
+        expect(diffusionMean).toBeWithin(0.45, 0.56)
+      })
+    })
+
+    describe('pre-image attacks resistance', () => {
+      let diffusionResults: Array<number>
+
+      beforeAll(() => diffusionResults = [])
+
+      // TODO: extract a test util to generate Shi7 options
+      it.each(([64, 128, 256, 512, 1024] as const)
+        .map(hashBitSize =>
+          Array.from({ length: 10 }, () => new SplitMix64().newSeed())
+            .map(seed => ({ seed, hashBitSize }))
+        )
+        .flat())
+        ('should differ largely for an empty message hash and the underlying SPD hash', ({ seed, hashBitSize }) => {
+          const hasher = new Shi7({ seed, hashBitSize })
+
+          const emptyHash = hasher.hash(empty)
+          const spdHash = hasher.hash(hasher.highSPD().readonlyBufferView())
+          diffusionResults.push(bitwiseDiffusion(emptyHash, spdHash))
+
+          expect(emptyHash).not.toBe(spdHash)
+        })
+
+      afterAll(() => {
+        const diffusionMean = diffusionResults
+          .reduce((acc, cur) => acc + cur, 0) / diffusionResults.length
+
+        expect(diffusionMean).toBeWithin(0.45, 0.56)
+      })
+    })
   })
+
   describe('hashing small messages in regard of hash bit size', () => {
     describe('collision resistance', () => { })
     describe('pre-image attacks resistance', () => { })
