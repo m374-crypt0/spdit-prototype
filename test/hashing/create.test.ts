cab2ac80717187e2e34eaea16b34159f5e5cd2f3
Sebastien Levy <m374-crypt0@pm.me>
2026-05-22 09:49:06 +0000

    test(hashing): refactoring

--- test/hashing/create.test.ts
+++ test/hashing/create.test.ts
@@ -1,9 +1,9 @@
 import { Shi7 } from "src/hashing";
-import { SplitMix64, UniformUint64 } from "src/stochastic";
-import { bitwiseDiffusion } from "./utils";
+import { SplitMix64, } from "src/stochastic";
+import { SPD, Transcoder } from "src/transcoding";
+import { bitwiseDiffusion, generateRandomUniqueMessages } from "./utils";
 
 import { afterAll, beforeAll, describe, expect, it } from "bun:test";
-import { SPD, Transcoder } from "src/transcoding";
 
 describe('hashing test suite', () => {
   describe('shi7 instantiation', () => {
@@ -205,28 +205,4 @@ describe('hashing test suite', () => {
 
 function generateSeeds(count: number) {
   return Array.from({ length: count }, () => new SplitMix64().newSeed())
-}
-
-function generateRandomUniqueMessages(options: GenerateRandomMessagesOptions) {
-  const { minSize, maxSize } = { minSize: BigInt(options.minSize), maxSize: BigInt(options.maxSize) }
-  const messageSet = new Set<string>
-  const d = new UniformUint64
-
-  for (let i = 0; i < options.maxCount; i++) {
-    const length = Number(d.newUint([minSize, maxSize]))
-    const a = Array.from({ length }, () => Number(d.newUint([0n, 255n])))
-    const s = a.reduce((acc, cur) => `${acc}${String.fromCharCode(cur)}`, '')
-    messageSet.add(s)
-  }
-
-  const messages = new Array<Readonly<Buffer<ArrayBuffer>>>
-  messageSet.values().forEach(m => messages.push(Buffer.from(m)))
-
-  return messages
-}
-
-type GenerateRandomMessagesOptions = {
-  minSize: number,
-  maxSize: number,
-  maxCount: number
 }
--- test/hashing/utils.ts
+++ test/hashing/utils.ts
@@ -1,7 +1,33 @@
+import { UniformUint64 } from "src/stochastic";
+
 export function bitwiseDiffusion(a: bigint, b: bigint) {
   const [_, max] = a < b ? [a, b] : [b, a]
   let bitCount = 0; for (let x = max; x > 0n; x >>= 1n, bitCount++);
   let distance = 0; for (let xor = a ^ b; xor > 0n; xor &= xor - 1n, distance++);
 
   return distance / bitCount
+}
+
+export function generateRandomUniqueMessages(options: GenerateRandomMessagesOptions) {
+  const { minSize, maxSize } = { minSize: BigInt(options.minSize), maxSize: BigInt(options.maxSize) }
+  const messageSet = new Set<string>
+  const d = new UniformUint64
+
+  for (let i = 0; i < options.maxCount; i++) {
+    const length = Number(d.newUint([minSize, maxSize]))
+    const a = Array.from({ length }, () => Number(d.newUint([0n, 255n])))
+    const s = a.reduce((acc, cur) => `${acc}${String.fromCharCode(cur)}`, '')
+    messageSet.add(s)
+  }
+
+  const messages = new Array<Readonly<Buffer<ArrayBuffer>>>
+  messageSet.values().forEach(m => messages.push(Buffer.from(m)))
+
+  return messages
+}
+
+type GenerateRandomMessagesOptions = {
+  minSize: number,
+  maxSize: number,
+  maxCount: number
 }
